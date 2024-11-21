ae3f58507197377f2e6f9ce3490f823b0af2f866
Dagdon <dagdonjephthah@gmail.com>
2024-11-21 07:17:14 +0100

    updates

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Task 5's module.
+'''Task 4's module.
 '''
-from typing import List
 
 
-def sum_list(input_list: List[float]) -> float:
-    '''Computes the sum of a list of floating-point numbers.
-    '''
-    return float(sum(input_list))
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
